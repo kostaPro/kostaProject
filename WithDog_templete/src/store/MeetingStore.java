9a3671f563862499a45f0a2ac297820a0365ba59
jakook <jakook413@naver.com>
2017-11-21 15:22:00 +0900

    commit

--- WithDog_templete/src/store/MeetingStore.java
+++ WithDog_templete/src/store/MeetingStore.java
@@ -1,5 +1,25 @@
 package store;
 
-public interface MeetingStore {
+import java.sql.Date;
+import java.util.List;
 
+import domain.Meeting;
+
+public interface MeetingStore {
+	
+	boolean createMeeting(Meeting meeting);
+	boolean retrieveAllMeetings();
+	List<Meeting> retrieveMeetingsByLocation(String loaction);
+	List<Meeting> retrieveMeetingsByDate(Date date);
+	List<Meeting> retrieveMeetingsByLocationDate(String location, Date date);
+	List<Meeting> retrieveMeetingsByGuest(String guestId);
+	List<Meeting> retrieveMeetingsByHost(String hostId);
+	Meeting retrieveMeetingByMeetingId(int meetingId);
+	List<String> retrieveJoinListByMeetingId(int meetingId);
+	List<String> retrieveImageListByMeetingId(int meetingId);
+	boolean joinMeeting(int meetingId, String guestId);
+	boolean cancelMeeting(int meetingId, String guestId);
+	boolean cancelAllJoinList(int meetingId);
+	boolean updateMeeting(Meeting meeting);
+	boolean deleteMeeting(int meetingId);
 }
--- WithDog_templete/src/store/SpotStore.java
+++ WithDog_templete/src/store/SpotStore.java
@@ -1,5 +1,24 @@
 package store;
 
-public interface SpotStore {
+import java.util.List;
 
+import domain.Spot;
+
+public interface SpotStore {
+	
+	boolean createSpot(Spot spot);
+	boolean createEventSpot(Spot spot);
+	List<Spot> retrieveAllSpots();
+	Spot retrieveEventSpot(int eventId);
+	List<Spot> retrieveSpotsByLocation(String location);
+	List<Spot> retrieveSpotsBySpotType(String spotType);
+	List<Spot> retrieveSpotsByLocationSpotType(String location, String spotType);
+	Spot retrieveSpotBySpotId(int spotId);
+	List<Spot> retrieveSpotsBySpotName(String spotName);
+	List<String> retrieveImageListBySpotId(int spotId);
+	List<Spot> retrieveSpotsByRegisterId(String registerId);
+	boolean updateSpot(Spot spot);
+	boolean updateEventSpot(Spot spot);
+	boolean deleteSpot(int spotId);
+	boolean deleteEventSpot(int eventId);
 }
