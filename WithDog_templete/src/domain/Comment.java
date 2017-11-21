e650946c911cef072d19395cfab54eb83d548a11
jakook <jakook413@naver.com>
2017-11-21 15:06:55 +0900

    commit

--- WithDog_templete/src/domain/Comment.java
+++ WithDog_templete/src/domain/Comment.java
@@ -1,5 +1,45 @@
 package domain;
 
+import java.sql.Date;
+import java.util.List;
+
 public class Comment {
+	
+	private int commentId;
+	private String writerId;
+	private String content;
+	private Date registDate;
+	private List<Comment> replyList;
+	
+	public int getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	public String getWriterId() {
+		return writerId;
+	}
+	public void setWriterId(String writerId) {
+		this.writerId = writerId;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegistDate() {
+		return registDate;
+	}
+	public void setRegistDate(Date registDate) {
+		this.registDate = registDate;
+	}
+	public List<Comment> getReplyList() {
+		return replyList;
+	}
+	public void setReplyList(List<Comment> replyList) {
+		this.replyList = replyList;
+	}
 
 }
--- WithDog_templete/src/domain/Report.java
+++ WithDog_templete/src/domain/Report.java
@@ -1,5 +1,42 @@
 package domain;
 
 public class Report {
+	
+	private String reportContents;
+	private String reportType;
+	private int reportTargetId;
+	private String reporterId;
+	private String status;
+	
+	public String getReportContents() {
+		return reportContents;
+	}
+	public void setReportContents(String reportContents) {
+		this.reportContents = reportContents;
+	}
+	public String getReportType() {
+		return reportType;
+	}
+	public void setReportType(String reportType) {
+		this.reportType = reportType;
+	}
+	public int getReportTargetId() {
+		return reportTargetId;
+	}
+	public void setReportTargetId(int reportTargetId) {
+		this.reportTargetId = reportTargetId;
+	}
+	public String getReporterId() {
+		return reporterId;
+	}
+	public void setReporterId(String reporterId) {
+		this.reporterId = reporterId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
 }
