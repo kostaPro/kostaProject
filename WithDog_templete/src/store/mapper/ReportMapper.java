9c4ae491a806eae2c7db58a689e52f19a9e8daa9
meimei712 <sungmikim301@gmail.com>
2017-11-21 17:09:52 +0900

    add Mapper Interface

--- WithDog_templete/src/store/mapper/ReportMapper.java
+++ WithDog_templete/src/store/mapper/ReportMapper.java
@@ -1,5 +1,25 @@
 package store.mapper;
 
-public interface ReportMapper {
+import java.util.List;
+import java.util.Map;
 
+import domain.Report;
+
+public interface ReportMapper {
+	
+	boolean createRepot(Report report);
+	
+	List<Report> retrieveReportsByReporterId(String reporterId);
+	List<Report> retrieveReportsBySuspectId(String suspectId);
+	
+	List<Report> retrieveAllReports();
+	
+	List<Report> retrieveReportsByReportType(String reportType);
+	List<String> retrieveBlackList();
+	
+	Report retrieveReport(Map<String,Object> map);
+	
+	boolean updateReport(Report report);
+	
+	boolean deleteReport(int reportId);
 }
--- WithDog_templete/src/store/mapper/SpotMapper.java
+++ WithDog_templete/src/store/mapper/SpotMapper.java
@@ -1,5 +1,29 @@
 package store.mapper;
 
-public interface SpotMapper {
+import java.util.List;
+import java.util.Map;
 
+import domain.Spot;
+
+public interface SpotMapper {
+	
+	boolean createSpot(Spot spot);
+	
+	List<Spot> retrieveAllSpots();
+	
+	Spot retrieveEventSpot(int eventId);
+	
+	List<Spot> retrieveSpotsByLocation(String location);
+	List<Spot> retrieveSpotsBySpotType(String spotType);
+	List<Spot> retrieveSpotsByLocationSpotType(Map<String, String> map);
+	
+	Spot retrieveSpotBySpotId(int spotId);
+	
+	List<Spot> retrieveSpotsBySpotName(String spotName);
+	List<String> retrieveImageListBySpotId(int spotId);
+	List<Spot> retrieveSpotsByRegisterId(String registerId);
+	
+	boolean updateSpot(Spot spot);
+	
+	boolean deleteSpot(int spotId);
 }
