984ebeb681bc3b251433d32c9c7131d6fc6a80e1
uraid <uraid@DESKTOP-8M3PIUF>
2017-11-28 11:06:15 +0900

    registUser.jsp_1128

--- WithDog_templete/src/controller/UserController.java
+++ WithDog_templete/src/controller/UserController.java
@@ -1,9 +1,14 @@
 package controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import domain.User;
 import service.UserService;
 
 @Controller
@@ -17,4 +22,30 @@ public class UserController {
 		return "redirect:login.jsp";
 	}
 
+	
+	@RequestMapping(value = "/registUser.do", method = RequestMethod.GET)
+	public String showRegistUser() {
+		
+		return "registUser.jsp";
+	}
+	
+//	@RequestMapping(value = "registUser.do", method = RequestMethod.POST)
+//	public String registUser(User user)  {
+//		
+//		boolean registered = userService.registUser(user);
+//		
+//		
+//		if(!registered) {
+//			ModelAndView modelAndView = new ModelAndView("registUser.jsp");
+//			return "redirect:registerUser.do";
+//		}else {
+//			return "redirect:login.do";
+//		}
+//	}
+	
+	@RequestMapping("/login.do")
+	public String showLogin() {
+		return "login.jsp";
+	}
+	
 }
