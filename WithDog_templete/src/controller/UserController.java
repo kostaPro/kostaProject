f2c3f711860f3cd519399ac8515d137468bba007
uraid <uraid@DESKTOP-8M3PIUF>
2017-11-29 09:07:39 +0900

    User_Controller1129

--- WithDog_templete/src/controller/UserController.java
+++ WithDog_templete/src/controller/UserController.java
@@ -1,11 +1,20 @@
 package controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import domain.User;
@@ -29,23 +38,84 @@ public class UserController {
 		return "registUser.jsp";
 	}
 	
-//	@RequestMapping(value = "registUser.do", method = RequestMethod.POST)
-//	public String registUser(User user)  {
+	@RequestMapping(value = "/registUser.do", method = RequestMethod.POST)
+	public String registUser(User user, MultipartHttpServletRequest file) throws IOException  {
+		
+//		String realFolder = "c:\\" + File.separator + "tempFiles";
+//		File dir = new File(realFolder);
+//		if(!dir.isDirectory()) {
+//			dir.mkdirs();
+//		}
 //		
-//		boolean registered = userService.registUser(user);
-//		
-//		
+//		MultipartFile petImage = file.getFile("petImage");
+//		System.out.println(petImage.getOriginalFilename());
+//		if(petImage == null && petImage.getOriginalFilename().equals("")) {
+//			
+//		}else {
+//			// 파일 중복명 처리
+//			String genId = UUID.randomUUID().toString();
+//			// 본래 파일명
+//			String originalfileName = petImage.getOriginalFilename();
+//			// 저장되는 파일 이름
+//			String saveFileName = genId + "." + originalfileName;
+//
+//			File saveFile = new File(dir.getAbsolutePath() + File.separator + saveFileName);
+//
+//			byte[] bytes = petImage.getBytes();
+//
+//			BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(saveFile));
+//			out.write(bytes);
+//			out.close(); 
+//			
+//			user.setPetImage(saveFileName);
+//		}
+//		System.out.println(user.getPetImage());
+		
+//		boolean registered =
+		user.setPetImage("fffff");
+		 userService.registUser(user);
+		
+		return "redirect:login.jsp";
 //		if(!registered) {
-//			ModelAndView modelAndView = new ModelAndView("registUser.jsp");
-//			return "redirect:registerUser.do";
+//			return "redirect:registUser.do";
 //		}else {
 //			return "redirect:login.do";
 //		}
-//	}
+	}
 	
 	@RequestMapping("/login.do")
 	public String showLogin() {
 		return "login.jsp";
 	}
 	
+	@RequestMapping("/removeUser.do")
+	public String removeUser(String userId) {
+		
+		userService.removeUser(userId);
+		
+		return "어디로 갈까?";
+	}
+	
+	@RequestMapping("/logout.do")
+	public String logout(HttpServletRequest req) {
+		
+		req.getSession().invalidate();
+		return "redirect:login.do";
+	}
+	
+	public ModelAndView showModifyUser(HttpServletRequest req) {
+		
+		HttpSession session = req.getSession();
+		
+		ModelAndView modelAndView = new ModelAndView("modifyUser.jsp");
+		modelAndView.addObject("user", userService.findUserByUserId(session.getId()));
+		
+		return modelAndView;
+	}
+	
+	public String modifyUser(User user) {
+		userService.modifyUser(user);
+		
+		return "redirect:마이페이지 어딘가로 갑시다";
+	}
 }
